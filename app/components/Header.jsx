249d69dbe87bfabd2e7748a68e6b656933487254
Nayem Islam <53568454+nayem-miah@users.noreply.github.com>
2024-05-14 17:52:32 +0600

    'commited'

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,16 +1,26 @@
-import { auth,signIn, signOut} from "@/auth"
-import Image from "next/image"
-import Link from "next/link"
-
-
+import { auth, signIn, signOut } from "@/auth";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function Header() {
+  const session = await auth();
+  console.log(".............", session);
+  return <div>
+    Headers
 
-    const session = await auth();
-    console.log('.............', session)
-  return (
-    <div>
-      Headers
-    </div>
-  )
+    {session?.user?(
+        <div>
+          <p>{session?.user?.name}</p>
+          <Image src={session?.user?.image}
+          alt={session?.user?.image}
+          width={32}
+          height={32}
+          className="rounded-full"
+          />
+        </div>
+    ):(
+    <p>Ask to login</p>
+    )}
+    
+    </div>;
 }
